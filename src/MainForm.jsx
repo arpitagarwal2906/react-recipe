ed3bbe6425be4d6fe2da0de05bfee585b5b0970e
Arpit Agarwal <arpag2906@gmail.com>
2026-05-03 16:02:45 +0530

    Implement ingredient addition functionality in MainForm component

--- src/MainForm.jsx
+++ src/MainForm.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
+
 export default function MainForm() {
+  const [ingredientsState, setingredientsState] = useState([]);
+
+  const ingredientsListItems = ingredientsState.map((ingredients, index) => (
+    <li key={index}>{ingredients}</li>
+  ));
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newIngredients = e.target.ingredient.value;
+    setingredientsState([...ingredientsState, newIngredients]);
+  }
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="e.g., oregano"
           aria-label="Add Ingredient"
+          name="ingredient"
         />
         <button>Add Ingredient</button>
       </form>
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 }
