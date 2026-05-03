da5c8ee4a97f3e88c6336e17b1d5d90f0f7aeff3
Arpit Agarwal <arpag2906@gmail.com>
2026-05-03 17:03:55 +0530

    Refactor form submission

--- src/MainForm.jsx
+++ src/MainForm.jsx
@@ -7,14 +7,22 @@ export default function MainForm() {
     <li key={index}>{ingredients}</li>
   ));
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const newIngredients = e.target.ingredient.value;
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   const formEl = e.currentTarget;
+  //   const newIngredients = e.currentTarget.ingredient.value;
+  //   setingredientsState([...ingredientsState, newIngredients]);
+  //   formEl.reset();
+  // }
+
+  function signUp(formData) {
+    const newIngredients = formData.get("ingredient");
     setingredientsState([...ingredientsState, newIngredients]);
   }
+
   return (
     <main>
-      <form className="add-ingredient-form" onSubmit={handleSubmit}>
+      <form className="add-ingredient-form" action={signUp}>
         <input
           type="text"
           placeholder="e.g., oregano"
